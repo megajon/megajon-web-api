64f7a88310e819b286560f91c7eaf72bfb49894a
megajon <jonathan.seubert@megajon.com>
2021-12-23 13:20:34 -0800

    first commit

--- schema.ts
+++ schema.ts
@@ -1,24 +1,5 @@
-/*
-Welcome to the schema! The schema is the heart of Keystone.
-
-Here we define our 'lists', which will then be used both for the GraphQL
-API definition, our database tables, and our Admin UI layout.
-
-Some quick definitions to help out:
-A list: A definition of a collection of fields with a name. For the starter
-  we have `User`, `Post`, and `Tag` lists.
-A field: The individual bits of data on your list, each with its own type.
-  you can see some of the lists in what we use below.
-
-*/
-
-// Like the `config` function we use in keystone.ts, we use functions
-// for putting in our config so we get useful errors. With typescript,
-// we get these even before code runs.
+import 'dotenv/config';
 import { list } from '@keystone-6/core';
-
-// We're using some common fields in the starter. Check out https://keystonejs.com/docs/apis/fields#fields-api
-// for the full list of fields.
 import {
   text,
   relationship,
@@ -26,21 +7,11 @@ import {
   timestamp,
   select,
 } from '@keystone-6/core/fields';
-// The document field is a more complicated field, so it's in its own package
-// Keystone aims to have all the base field types, but you can make your own
-// custom ones.
 import { document } from '@keystone-6/fields-document';
-import { cloudinaryImage } from '@keystone-6/cloudinary';
-import 'dotenv/config';
 
-// We have a users list, a blogs list, and tags for blog posts, so they can be filtered.
-// Each property on the exported object will become the name of a list (a.k.a. the `listKey`),
-// with the value being the definition of the list, including the fields.
+
 export const lists = {
-  // Here we define the user list.
   User: list({
-    // Here are the fields that `User` will have. We want an email and password so they can log in
-    // a name so we can refer to them, and a way to connect users to posts.
     fields: {
       name: text({ validation: { isRequired: true } }),
       email: text({
@@ -48,15 +19,9 @@ export const lists = {
         isIndexed: 'unique',
         isFilterable: true,
       }),
-      // The password field takes care of hiding details and hashing values
       password: password({ validation: { isRequired: true } }),
-      // Relationships allow us to reference other lists. In this case,
-      // we want a user to have many posts, and we are saying that the user
-      // should be referencable by the 'author' field of posts.
-      // Make sure you read the docs to understand how they work: https://keystonejs.com/docs/guides/relationships#understanding-relationships
       posts: relationship({ ref: 'Post.author', many: true }),
     },
-    // Here we can configure the Admin UI. We want to show a user's name and posts in the Admin UI
     ui: {
       listView: {
         initialColumns: ['name', 'posts'],
@@ -67,38 +32,36 @@ export const lists = {
     fields: {
       name: text(),
       title: text(),
-      image: cloudinaryImage({
-        cloudinary: {
-          cloudName: 'megajon-com',
-          apiKey: '327795115734343',
-          apiSecret: 'pI7bZhZo5IC6sOH6Wvhg44Dzb_c',
-          folder: 'megajon-hero',
-        },
-      }),
+      image: text(),
     }
   }),
-  // Our second list is the Posts list. We've got a few more fields here
-  // so we have all the info we need for displaying posts.
+  Logo: list({
+    fields: {
+      name: text(),
+      image_url: text(),
+      href: text(),
+    }
+  }),
+  Icon: list({
+    fields: {
+      name: text(),
+      image_url: text(),
+      href: text(),
+    }
+  }),
   Post: list({
     fields: {
       title: text(),
-      // Having the status here will make it easy for us to choose whether to display
-      // posts on a live site.
       status: select({
         options: [
           { label: 'Published', value: 'published' },
           { label: 'Draft', value: 'draft' },
         ],
-        // We want to make sure new posts start off as a draft when they are created
         defaultValue: 'draft',
-        // fields also have the ability to configure their appearance in the Admin UI
         ui: {
           displayMode: 'segmented-control',
         },
       }),
-      // The document field can be used for making highly editable content. Check out our
-      // guide on the document field https://keystonejs.com/docs/guides/document-fields#how-to-use-document-fields
-      // for more information
       content: document({
         formatting: true,
         layouts: [
@@ -112,8 +75,6 @@ export const lists = {
         dividers: true,
       }),
       publishDate: timestamp(),
-      // Here is the link from post => author.
-      // We've configured its UI display quite a lot to make the experience of editing posts better.
       author: relationship({
         ref: 'User.posts',
         ui: {
@@ -124,7 +85,6 @@ export const lists = {
           inlineCreate: { fields: ['name', 'email'] },
         },
       }),
-      // We also link posts to tags. This is a many <=> many linking.
       tags: relationship({
         ref: 'Tag.posts',
         ui: {
@@ -139,7 +99,6 @@ export const lists = {
       }),
     },
   }),
-  // Our final list is the tag list. This field is just a name and a relationship to posts
   Tag: list({
     ui: {
       isHidden: true,
